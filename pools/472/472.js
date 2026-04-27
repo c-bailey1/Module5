a3b97a57115a3f1ed1f3b873460716d969fb4a95
c-bailey1 <christopher.bailey@quinnipiac.edu>
2026-04-27 11:49:19 -0400

    Add files via upload

---
+++ pools/472/472.js
@@ -0,0 +1,17 @@
+window.CPPool472Init = function() { cp.model['472Data']={
+};cp.poolSlideResources['pool472']={
+};
+cp.model.data['472']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["472Images"]=[];
+cp.poolResources["472Videos"]=[
+];
+cp.poolResources["472SlideVideos"]=[
+];
+}
